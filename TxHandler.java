8cddf44e8ffcdf0bb4879b9026f2872521e542d8
Jonathan <jonzhus@icloud.com>
2026-02-01 14:24:30 +0000

    Fixed HashSet<>() Diamond operator syntax

--- TxHandler.java
+++ TxHandler.java
@@ -1,6 +1,7 @@
 import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class TxHandler {
@@ -12,7 +13,7 @@ public class TxHandler {
      */
 
     // Scrooge's private record of the public ledger
-    private UTXOPool scroogePool;
+    private final UTXOPool scroogePool;
 
     public TxHandler(UTXOPool utxoPool) {
         //(declare a new ledger - done as a private attribute of class TxHandler)
@@ -32,12 +33,11 @@ public class TxHandler {
     public boolean isValidTx(Transaction tx) {
         //(1)return false if any of tx.input is not in scroogePool
         //loop through all inputs
-        ArrayList<Transaction.Input> all_inputs = tx.getInputs();
-        Set<UTXO> seen;
+        Set<UTXO> seen = new HashSet<>();
         double input_sum = 0;
         for (int i = 0; i < tx.numInputs() ; i++) {
             //UTXO constructor
-            UTXO inUTXO = new UTXO(all_inputs.get(i).prevTxHash, all_inputs.get(i).outputIndex);
+            UTXO inUTXO = new UTXO(tx.getInput(i).prevTxHash, tx.getInput(i).outputIndex);
             input_sum+= this.scroogePool.getTxOutput(inUTXO).value;
 
             //retrieve the public key from the output of this UTXO
@@ -46,7 +46,7 @@ public class TxHandler {
                 return false; //means the input UTXO is not in the pool
             }
             //(2) check signature on previous transaction matches the public key of the sender of this transaction
-            if(!Crypto.verifySignature(out.address, tx.getRawDataToSign(i), all_inputs.get(i).signature){
+            if(!Crypto.verifySignature(out.address, tx.getRawDataToSign(i), tx.getInput(i).signature ) ){
                 return false;
             }
             //(3) check if UTXOs in tx is duplicated
@@ -57,14 +57,13 @@ public class TxHandler {
             }
         }
         //finished checking inputs, now check outputs
-        ArrayList<Transaction.Output> all_outputs = tx.getOutputs();
         double output_sum = 0;
         for (int i =  0; i< tx.numOutputs(); i++){
             //(4) non-negative values
-            if (all_outputs.get(i).value < 0 ) {
+            if (tx.getOutput(i).value < 0 ) {
                 return false;
             }
-            output_sum += all_outputs.get(i).value;
+            output_sum += tx.getOutput(i).value;
         }
         //(5)the sum of {@code tx}s input values is greater than or equal to the sum of its output
         return (input_sum >= output_sum);
