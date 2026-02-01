28f349f613478a0fbb07a8497afbbbe08d7690fc
Jonathan <jonzhus@icloud.com>
2026-02-01 14:18:33 +0000

    Addded a partial solution to the TxHandler class.
    Completed transaction constructor and transaction validation to prevent
    double spending and spending non-existent coins.

--- TxHandler.java
+++ TxHandler.java
@@ -1,3 +1,8 @@
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+
 public class TxHandler {
 
     /**
@@ -5,8 +10,14 @@ public class TxHandler {
      * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
      * constructor.
      */
+
+    // Scrooge's private record of the public ledger
+    private UTXOPool scroogePool;
+
     public TxHandler(UTXOPool utxoPool) {
-        // IMPLEMENT THIS
+        //(declare a new ledger - done as a private attribute of class TxHandler)
+        //Assign a deep copy of utxoPool to scroogePool
+        this.scroogePool = new UTXOPool(utxoPool);
     }
 
     /**
@@ -19,7 +30,44 @@ public class TxHandler {
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        // IMPLEMENT THIS
+        //(1)return false if any of tx.input is not in scroogePool
+        //loop through all inputs
+        ArrayList<Transaction.Input> all_inputs = tx.getInputs();
+        Set<UTXO> seen;
+        double input_sum = 0;
+        for (int i = 0; i < tx.numInputs() ; i++) {
+            //UTXO constructor
+            UTXO inUTXO = new UTXO(all_inputs.get(i).prevTxHash, all_inputs.get(i).outputIndex);
+            input_sum+= this.scroogePool.getTxOutput(inUTXO).value;
+
+            //retrieve the public key from the output of this UTXO
+            Transaction.Output out = this.scroogePool.getTxOutput(inUTXO);
+            if (out == null){
+                return false; //means the input UTXO is not in the pool
+            }
+            //(2) check signature on previous transaction matches the public key of the sender of this transaction
+            if(!Crypto.verifySignature(out.address, tx.getRawDataToSign(i), all_inputs.get(i).signature){
+                return false;
+            }
+            //(3) check if UTXOs in tx is duplicated
+            int count_before = seen.size();
+            seen.add(inUTXO);
+            if (seen.size() == count_before){
+                return false;
+            }
+        }
+        //finished checking inputs, now check outputs
+        ArrayList<Transaction.Output> all_outputs = tx.getOutputs();
+        double output_sum = 0;
+        for (int i =  0; i< tx.numOutputs(); i++){
+            //(4) non-negative values
+            if (all_outputs.get(i).value < 0 ) {
+                return false;
+            }
+            output_sum += all_outputs.get(i).value;
+        }
+        //(5)the sum of {@code tx}s input values is greater than or equal to the sum of its output
+        return (input_sum >= output_sum);
     }
 
     /**
