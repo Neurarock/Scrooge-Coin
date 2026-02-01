1eed3a6459c40a6ca43e9d74fee94e2bf2e5e98e
Jonathan <jonzhus@icloud.com>
2026-02-01 16:38:05 +0000

    (feat) Minimum working Version for TxHandler pending Max Fee Handler

--- TxHandler.java
+++ TxHandler.java
@@ -1,5 +1,6 @@
 import java.security.PublicKey;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,12 +14,26 @@ public class TxHandler {
      */
 
     // Scrooge's private record of the public ledger
-    private final UTXOPool scroogePool;
+    private UTXOPool scroogePool;
 
     public TxHandler(UTXOPool utxoPool) {
         //(declare a new ledger - done as a private attribute of class TxHandler)
         //Assign a deep copy of utxoPool to scroogePool
         this.scroogePool = new UTXOPool(utxoPool);
+    }
+
+    //if fee is negative transaction is invalid
+    public double fee(Transaction tx) {
+        double output_sum = 0;
+        for (int i =  0; i< tx.numOutputs(); i++){
+            output_sum += tx.getOutput(i).value;
+        }
+        double input_sum = 0;
+        for (int i =  0; i< tx.numInputs(); i++){
+            UTXO inUTXO = new UTXO(tx.getInput(i).prevTxHash, tx.getInput(i).outputIndex);
+            input_sum+= this.scroogePool.getTxOutput(inUTXO).value;
+        }
+        return (input_sum - output_sum);
     }
 
     /**
@@ -69,13 +84,46 @@ public class TxHandler {
         return (input_sum >= output_sum);
     }
 
+    //helper to change the status of the UTXO Pool (Assume validity already checked)
+    public void spendUTXO(Transaction tx) {
+        //spend inputs
+        for (int i = 0; i < tx.numInputs() ; i++) {
+            UTXO inUTXO = new UTXO(tx.getInput(i).prevTxHash, tx.getInput(i).outputIndex);
+            this.scroogePool.removeUTXO(inUTXO);//spend all of these UTXOs
+        }
+        //add outputs to Pool
+        for (int i = 0; i < tx.numOutputs() ; i++) {
+            UTXO leftOverUTXO = new UTXO(tx.getHash(), i);
+            this.scroogePool.addUTXO(leftOverUTXO, tx.getOutput(i));//add new ones to pool
+        }
+    }
+
+
+
     /**
      * Handles each epoch by receiving an unordered array of proposed transactions, checking each
      * transaction for correctness, returning a mutually valid array of accepted transactions, and
      * updating the current UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
-        // IMPLEMENT THIS
+        // Extract a list of valid(by itself) transactions (correct)
+        ArrayList<Transaction> acceptedTx = new ArrayList<>();
+        for (Transaction t : possibleTxs){
+            if (!this.isValidTx(t)) {
+                continue;
+            }
+            //if it is valid, spend the coins and add to acceptedTx
+            //spend in UTXO logic
+            spendUTXO(t);
+            acceptedTx.add(t);
+        }
+        return acceptedTx.toArray(new Transaction[acceptedTx.size()]);
     }
+
+    /*
+     * For maximum fee
+     * Recursively search through all possible combinations
+     * of transactions and choose the one with the highest fee
+     */
 
 }
